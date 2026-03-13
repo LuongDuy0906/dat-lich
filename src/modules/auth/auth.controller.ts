1526e82ba4ff791606d55b1097b394f5759d65df
luongduy0906 <luongducduy0906@gmail.com>
2026-03-13 14:19:08 +0700

    Them that vai thu

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { RefreshAuthGuard } from './guards/refresh-auth/refresh-auth.guard';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags("auth")
 @ApiBearerAuth()
@@ -11,11 +11,17 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+    @ApiOperation({
+      summary: "API dành cho việc đăng nhập"
+    })
     @Post('login')
     login(@Body() loginDto: LoginDto){
         return this.authService.login(loginDto);
     }
 
+     @ApiOperation({
+      summary: "API dành cho việc đăng ký tài khoản"
+    })
     @Post('register')
     register(@Body() createUserDto: CreateUserDto){
       return this.authService.register(createUserDto);
